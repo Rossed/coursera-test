9d15475294ecc49117caa5b51c850b32b5d7fdd0
Rossed <ross.edwards@hotmail.com.au>
2016-08-01 23:20:04 +1000

    progress on main page, mostly side-bar but not complete

--- holroydMed/js/script.js
+++ holroydMed/js/script.js
@@ -1,5 +1,6 @@
 (function (global) {
 
+
 	var hlryd = {};
 
 	var homeHtmlUrl = "snippets/home-snippet.html";
@@ -36,6 +37,42 @@
     			insertHtml("#changable-content", homeHtml);
     		}, false);
 	});
+
+	var menu = $('#side-bar');
+	var menuY = menu.offset().top;
+	var $w = $(window);	
+	$w.scroll(function() {
+        var scrollTop = $w.scrollTop();
+        var footerTop = document.querySelector("#footer").getBoundingClientRect().top;
+        var sideBarBottom = document
+        	.querySelector("#side-bar").getBoundingClientRect().bottom;
+        	console.log("bottom: "+sideBarBottom);
+        	console.log("top: " +footerTop);
+        	console.log("-----")
+    	if (footerTop - 50 > sideBarBottom) {
+	        menu.css({
+	        	top: scrollTop
+	        });
+    	}
+
+        // var shouldBeFixed = scrollTop > menuY;
+        // if (shouldBeFixed && !isFixed) {
+        //     menu.css({
+        //         position: 'fixed',
+        //         top: 0,
+        //         left: menu.offset().left,
+        //         width: menu.width()
+        //     });
+        //     isFixed = true;
+        // }
+        // else if (!shouldBeFixed && isFixed)
+        // {
+        //     menu.css({
+        //         position: 'static'
+        //     });
+        //     isFixed = false;
+        // }
+    });
 
 	global.$hlryd = hlryd;
 
