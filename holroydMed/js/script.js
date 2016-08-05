db36d4c1ccd58935f53597bdaafbd16d2bc084fb
Rossed <ross.edwards@hotmail.com.au>
2016-08-05 21:15:16 +1000

    links page added

--- holroydMed/js/script.js
+++ holroydMed/js/script.js
@@ -3,8 +3,9 @@
 
 	var hlryd = {};
 
-	var homeHtmlUrl = "snippets/home-snippet.html";
-	var histroyHTMLUrl = "snippets/history-snippet.html"
+	var homeHtmlUrl = "snippets/home-snippet.html",
+		histroyHTMLUrl = "snippets/history-snippet.html",
+		linkHTMLUrl = "snippets/links-snippet.html";
 
 	// Convenience function for inserting innerHTML for 'select'
 	var insertHtml = function (selector, html) {
@@ -21,10 +22,13 @@
 
 	// Changes active menu option on side-bar
 	hlryd.selected = function (keyWord) {
-		firstLoad = true;
+
 		hlryd.loadPage(keyWord);
 		document.getElementsByClassName('selected')[0].className = "";
 		document.getElementById(keyWord).className += " selected";
+
+		$(window).load(function(){loaded=true});
+		console.log(loaded);
 	};
 
 	// On page load (before images or CSS)
@@ -39,6 +43,7 @@
     		function (homeHtml) {
     			insertHtml("#changable-content", homeHtml);
     		}, false);
+
 	});
 
 	hlryd.loadPage = function (keyWord) {
@@ -47,6 +52,8 @@
 			url = homeHtmlUrl;
 		} else if (keyWord === 'History') {
 			url = histroyHTMLUrl;
+		} else if (keyWord === 'Links') {
+			url = linkHTMLUrl;
 		}
 		showLoading("#changable-content");
 		$ajaxUtils.sendGetRequest(
@@ -57,52 +64,51 @@
 
 	}
 
-	// Declared variables for scrolling side-bar
-	var $sidebar = $("#side-bar"),
-    	$window    = $(window),
-    	offset     = $sidebar.offset(),
-    	topPadding = 60,
-    	sidebarPadding = parseInt($("#side-bar").css('padding-top')) +
-    		parseInt($("#side-bar").css('padding-bottom')) + 2,
-    	firstLoad = true;
-	var documentHeight,
-    		$footer,
-    		footerOffset,
-    		footerMargin;
+	// // Declared variables for scrolling side-bar
+	// var $sidebar = $("#side-bar"),
+ //    	$window    = $(window),
+ //    	offset     = $sidebar.offset(),
+ //    	topPadding = 60,
+ //    	sidebarPadding = parseInt($("#side-bar").css('padding-top')) +
+ //    		parseInt($("#side-bar").css('padding-bottom')) + 2,
+ //    	loaded = false;
+	// var documentHeight,
+	// 	$footer,
+	// 	footerOffset,
+	// 	footerMargin;
 
+	// 	$(window).load(function(){loaded=true});
     	
-    //Function that ensure side-bar follow when scrolling
-    $window.scroll(function() {
+ //    //Function that ensure side-bar follow when scrolling
+ //    $window.scroll(function() {
 
-    	if (firstLoad) {
-	    	documentHeight = ($(document).height());
-    		$footer = $("#footer");
-    		footerOffset = $footer.offset(),
-    		footerMargin = parseInt($footer.css('margin-top')),
-    		firstLoad = false;
-    	}
+ //    	if (loaded) {
+	//     	documentHeight = ($(document).height());
+ //    		$footer = $("#footer");
+ //    		footerOffset = $footer.offset();
+ //    		footerMargin = parseInt($footer.css('margin-top'));
+ //    		loaded = false;
+ //    	}
+
+ //    	console.log(footerOffset.top);
 
 
-        if ($window.scrollTop() > (offset.top - topPadding)) {
-    		if ($window.scrollTop() < (footerOffset.top - topPadding - $sidebar.height() - sidebarPadding - footerMargin)) {
-            	$sidebar.stop().animate({
-                	marginTop: $window.scrollTop() - offset.top + topPadding
-                })
-            } else {
-            	console.log($window.scrollTop()),
-            	console.log($sidebar.css('margin-top')),
-            	console.log("------"),
-            	console.log("(" + footerOffset.top + ")  - " + $sidebar.height() + " - " + sidebarPadding + " - " + footerMargin),
-            	$sidebar.stop().animate({
-            		marginTop: ((footerOffset.top) - $sidebar.height() - sidebarPadding - footerMargin - offset.top)
-            	})
-            }
-    	} else {
-            $sidebar.stop().animate({
-                marginTop: 0
-            });
-        }
-    	});
+ //        if ($window.scrollTop() > (offset.top - topPadding)) {
+ //    		if ($window.scrollTop() < (footerOffset.top - topPadding - $sidebar.height() - sidebarPadding - footerMargin)) {
+ //            	$sidebar.stop().animate({
+ //                	marginTop: $window.scrollTop() - offset.top + topPadding
+ //                })
+ //            } else {
+ //            	$sidebar.stop().animate({
+ //            		marginTop: ((footerOffset.top) - $sidebar.height() - sidebarPadding - footerMargin - offset.top)
+ //            	})
+ //            }
+ //    	} else {
+ //            $sidebar.stop().animate({
+ //                marginTop: 0
+ //            });
+ //        }
+ //    	});
    		
 
 
