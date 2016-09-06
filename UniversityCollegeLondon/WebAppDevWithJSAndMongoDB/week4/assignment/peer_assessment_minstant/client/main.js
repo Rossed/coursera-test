421aa5e729be0acc4d95376a93fa830689cfbae9
Rossed <ross.edwards@hotmail.com.au>
2016-09-06 20:26:26 +1000

    usernames must be less than 8 chars long added

--- UniversityCollegeLondon/WebAppDevWithJSAndMongoDB/week4/assignment/peer_assessment_minstant/client/main.js
+++ UniversityCollegeLondon/WebAppDevWithJSAndMongoDB/week4/assignment/peer_assessment_minstant/client/main.js
@@ -22,29 +22,34 @@ Router.route('/', function () {
 Router.route('/chat/:_id', function () {
   // the user they want to chat to has id equal to 
   // the id sent in after /chat/... 
-  var otherUserId = this.params._id;
-  // find a chat that has two users that match current user id
-  // and the requested user id
-  var filter = {$or:[
-              {user1Id:Meteor.userId(), user2Id:otherUserId}, 
-              {user2Id:Meteor.userId(), user1Id:otherUserId}
-              ]};
-  var chat = Chats.findOne(filter);
-  if (Session.get('db_data_loaded')) {
-    if (!chat){// no chat matching the filter - need to insert a new one
-      chatId = Meteor.call('insertChat', otherUserId);
-      console.log("no chat matching the filter - need to insert a new one");
+  if (!Meteor.user()) {
+    alert('Please log in to chat');
+    
+  } else {
+    var otherUserId = this.params._id;
+    // find a chat that has two users that match current user id
+    // and the requested user id
+    var filter = {$or:[
+                {user1Id:Meteor.userId(), user2Id:otherUserId}, 
+                {user2Id:Meteor.userId(), user1Id:otherUserId}
+                ]};
+    var chat = Chats.findOne(filter);
+    if (Session.get('db_data_loaded')) {
+      if (!chat){// no chat matching the filter - need to insert a new one
+        chatId = Meteor.call('insertChat', otherUserId);
+        console.log("no chat matching the filter - need to insert a new one");
+      }
+      else {// there is a chat going already - use that. 
+        console.log("there is a chat going already - use that.");
+        chatId = chat._id;
+      }
+      if (chatId){// looking good, save the id to the session
+        Session.set("chatId",chatId);
+      }
     }
-    else {// there is a chat going already - use that. 
-      console.log("there is a chat going already - use that.");
-      chatId = chat._id;
+      this.render("navbar", {to:"header"});
+      this.render("chat_page", {to:"main"}); 
     }
-    if (chatId){// looking good, save the id to the session
-      Session.set("chatId",chatId);
-    }
-}
-    this.render("navbar", {to:"header"});
-    this.render("chat_page", {to:"main"}); 
  });
 
 ///
@@ -221,6 +226,8 @@ Accounts.ui.config({
         if (!value) {
           errorFunction("Please enter a username");
           return false;
+        } else if(value.length >= 9) {
+          errorFunction("Please enter a username of 8 characters or less");
         } else {
           return true;
         }
